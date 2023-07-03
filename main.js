61212f96819d8911c04d13875e09ff462a138969
Manmohan Singh Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-07-03 12:13:22 +0530

    enums

--- main.js
+++ main.js
@@ -15,5 +15,27 @@
 /*
 ? Type
 */
-var count = 5;
-count = 'a';
+// let count = 5;
+// count = 'a';
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+    Color[Color["Purple"] = 3] = "Purple";
+})(Color || (Color = {}));
+; //What is enum? It is a way of giving more friendly names to sets of numeric values. 
+//more simpler enum definition is enum Color {Red, Green, Blue}; //It will automatically assign 0, 1, 2 to the enum members.
+//what if we give a value to the first member of the enum? enum Color {Red = 0, Green, Blue}; //It will automatically assign 0, 1, 2 to the enum members.
+//what if we give a value to the second member of the enum? enum Color {Red = 0, Green = 2, Blue}; //It will automatically assign 0, 2, 3 to the enum members.
+var backgroundColor = Color.Red;
+console.log(backgroundColor);
